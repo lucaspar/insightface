6a7bf4c2af08d4a7dfd8734e628df3ef4253c174
Lucas Parzianello <lucaspar@users.noreply.github.com>
2022-06-17 09:05:28 -0400

    arcface backbone improvements

--- recognition/arcface_torch/backbones/iresnet.py
+++ recognition/arcface_torch/backbones/iresnet.py
@@ -1,111 +1,151 @@
+"""Creation of ArcFace's ResNet-based models."""
+import sys
+
+sys.path.append("../../../")
+# pylint: disable=import-error,no-name-in-module
+from utils.logger import create_logger
+
+log = create_logger(__name__)
+from typing import Iterable, Optional, Tuple
+
 import torch
 from torch import nn
-from torch.utils.checkpoint import checkpoint
 
-__all__ = ['iresnet18', 'iresnet34', 'iresnet50', 'iresnet100', 'iresnet200']
-using_ckpt = False
+
+__all__ = ["iresnet18", "iresnet34", "iresnet50", "iresnet100", "iresnet200"]
+
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes,
-                     out_planes,
-                     kernel_size=3,
-                     stride=stride,
-                     padding=dilation,
-                     groups=groups,
-                     bias=False,
-                     dilation=dilation)
+    return nn.Conv2d(
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=dilation,
+        groups=groups,
+        bias=False,
+        dilation=dilation,
+    )
 
 
 def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
-    return nn.Conv2d(in_planes,
-                     out_planes,
-                     kernel_size=1,
-                     stride=stride,
-                     bias=False)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 
 class IBasicBlock(nn.Module):
+    """Basic Block for Identity ResNet"""
+
     expansion = 1
-    def __init__(self, inplanes, planes, stride=1, downsample=None,
-                 groups=1, base_width=64, dilation=1):
+
+    def __init__(
+        self,
+        in_planes,
+        planes,
+        stride=1,
+        downsample=None,
+        groups=1,
+        base_width=64,
+        dilation=1,
+    ):
         super(IBasicBlock, self).__init__()
         if groups != 1 or base_width != 64:
-            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+            raise ValueError("BasicBlock only supports groups=1 and base_width=64")
         if dilation > 1:
             raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
-        self.bn1 = nn.BatchNorm2d(inplanes, eps=1e-05,)
-        self.conv1 = conv3x3(inplanes, planes)
-        self.bn2 = nn.BatchNorm2d(planes, eps=1e-05,)
+        self.bn1 = nn.BatchNorm2d(
+            in_planes,
+            eps=1e-05,
+        )
+        self.conv1 = conv3x3(in_planes, planes)
+        self.bn2 = nn.BatchNorm2d(
+            planes,
+            eps=1e-05,
+        )
         self.prelu = nn.PReLU(planes)
         self.conv2 = conv3x3(planes, planes, stride)
-        self.bn3 = nn.BatchNorm2d(planes, eps=1e-05,)
+        self.bn3 = nn.BatchNorm2d(
+            planes,
+            eps=1e-05,
+        )
         self.downsample = downsample
         self.stride = stride
 
-    def forward_impl(self, x):
-        identity = x
-        out = self.bn1(x)
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the block."""
+        identity = input_tensor
+        out = self.bn1(input_tensor)
         out = self.conv1(out)
         out = self.bn2(out)
         out = self.prelu(out)
         out = self.conv2(out)
         out = self.bn3(out)
         if self.downsample is not None:
-            identity = self.downsample(x)
+            identity = self.downsample(input_tensor)
         out += identity
-        return out        
-
-    def forward(self, x):
-        if self.training and using_ckpt:
-            return checkpoint(self.forward_impl, x)
-        else:
-            return self.forward_impl(x)
+        return out
 
 
 class IResNet(nn.Module):
+    """Base class for Identity ResNet"""
+
     fc_scale = 7 * 7
-    def __init__(self,
-                 block, layers, dropout=0, num_features=512, zero_init_residual=False,
-                 groups=1, width_per_group=64, replace_stride_with_dilation=None, fp16=False):
-        super(IResNet, self).__init__()
-        self.extra_gflops = 0.0
+
+    def __init__(
+        self,
+        block: nn.Module,
+        layers: Iterable,
+        dropout: float = 0,
+        num_features: int = 512,
+        zero_init_residual: bool = False,
+        groups: int = 1,
+        width_per_group: int = 64,
+        replace_stride_with_dilation: Optional[Tuple[bool, bool, bool]] = None,
+        fp16: bool = False,
+        architecture: str = "arcface_1dcnn",  # arcface_1dcnn, arcface_2dcnn, arcface_3dcnn
+    ):
+        super().__init__()
         self.fp16 = fp16
-        self.inplanes = 64
+        self.in_planes = 64
         self.dilation = 1
         if replace_stride_with_dilation is None:
             replace_stride_with_dilation = [False, False, False]
         if len(replace_stride_with_dilation) != 3:
-            raise ValueError("replace_stride_with_dilation should be None "
-                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+            raise ValueError(
+                "replace_stride_with_dilation should be None "
+                "or a 3-element tuple, got {}".format(replace_stride_with_dilation)
+            )
         self.groups = groups
         self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(self.inplanes, eps=1e-05)
-        self.prelu = nn.PReLU(self.inplanes)
+        self.conv1 = nn.Conv2d(
+            3, self.in_planes, kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(self.in_planes, eps=1e-05)
+        self.prelu = nn.PReLU(self.in_planes)
         self.layer1 = self._make_layer(block, 64, layers[0], stride=2)
-        self.layer2 = self._make_layer(block,
-                                       128,
-                                       layers[1],
-                                       stride=2,
-                                       dilate=replace_stride_with_dilation[0])
-        self.layer3 = self._make_layer(block,
-                                       256,
-                                       layers[2],
-                                       stride=2,
-                                       dilate=replace_stride_with_dilation[1])
-        self.layer4 = self._make_layer(block,
-                                       512,
-                                       layers[3],
-                                       stride=2,
-                                       dilate=replace_stride_with_dilation[2])
-        self.bn2 = nn.BatchNorm2d(512 * block.expansion, eps=1e-05,)
+        self.layer2 = self._make_layer(
+            block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0]
+        )
+        self.layer3 = self._make_layer(
+            block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1]
+        )
+        self.layer4 = self._make_layer(
+            block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2]
+        )
+        self.bn2 = nn.BatchNorm2d(
+            512 * block.expansion,
+            eps=1e-05,
+        )
         self.dropout = nn.Dropout(p=dropout, inplace=True)
         self.fc = nn.Linear(512 * block.expansion * self.fc_scale, num_features)
         self.features = nn.BatchNorm1d(num_features, eps=1e-05)
         nn.init.constant_(self.features.weight, 1.0)
         self.features.weight.requires_grad = False
+
+        # Pareidolia change:
+        self.architecture = architecture
+        self.build_pipeline()
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -119,76 +159,134 @@ class IResNet(nn.Module):
                 if isinstance(m, IBasicBlock):
                     nn.init.constant_(m.bn2.weight, 0)
 
+    def build_pipeline(self):
+        """Builds the pipeline to forward the model inputs later."""
+        backbone = list(
+            [
+                self.conv1,
+                self.bn1,
+                self.prelu,
+                self.layer1,
+                self.layer2,
+                self.layer3,
+                self.layer4,
+                self.bn2,
+            ]
+        )
+        if self.architecture in ["arcface_1dcnn", "arcface_2dcnn"]:
+            self._pipeline = nn.Sequential(
+                *backbone,
+                nn.Flatten(start_dim=1),
+                nn.Dropout(p=0.5),
+                self.fc,
+                self.features,
+            )
+        elif self.architecture in ["arcface_3dcnn"]:
+            self._pipeline = nn.Sequential(
+                *backbone,
+            )
+
     def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        """Creates a layer of ResNet"""
         downsample = None
         previous_dilation = self.dilation
         if dilate:
             self.dilation *= stride
             stride = 1
-        if stride != 1 or self.inplanes != planes * block.expansion:
+        if stride != 1 or self.in_planes != planes * block.expansion:
             downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                nn.BatchNorm2d(planes * block.expansion, eps=1e-05, ),
+                conv1x1(self.in_planes, planes * block.expansion, stride),
+                nn.BatchNorm2d(
+                    planes * block.expansion,
+                    eps=1e-05,
+                ),
             )
         layers = []
         layers.append(
-            block(self.inplanes, planes, stride, downsample, self.groups,
-                  self.base_width, previous_dilation))
-        self.inplanes = planes * block.expansion
+            block(
+                self.in_planes,
+                planes,
+                stride,
+                downsample,
+                self.groups,
+                self.base_width,
+                previous_dilation,
+            )
+        )
+        self.in_planes = planes * block.expansion
         for _ in range(1, blocks):
             layers.append(
-                block(self.inplanes,
-                      planes,
-                      groups=self.groups,
-                      base_width=self.base_width,
-                      dilation=self.dilation))
+                block(
+                    self.in_planes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                )
+            )
 
         return nn.Sequential(*layers)
 
-    def forward(self, x):
-        with torch.cuda.amp.autocast(self.fp16):
-            x = self.conv1(x)
-            x = self.bn1(x)
-            x = self.prelu(x)
-            x = self.layer1(x)
-            x = self.layer2(x)
-            x = self.layer3(x)
-            x = self.layer4(x)
-            x = self.bn2(x)
-            x = torch.flatten(x, 1)
-            x = self.dropout(x)
-        x = self.fc(x.float() if self.fp16 else x)
-        x = self.features(x)
-        return x
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+        """Forward pass."""
+        features = self._pipeline(input_tensor)
+        # log.info(features.shape)
+        return features
 
 
-def _iresnet(arch, block, layers, pretrained, progress, **kwargs):
-    model = IResNet(block, layers, **kwargs)
-    if pretrained:
-        raise ValueError()
+def _iresnet(
+    block: nn.Module, layers: Iterable, architecture: str = "arcface_1dcnn", **kwargs
+):
+    """Constructs a ResNet-based model."""
+    model = IResNet(
+        block=block,
+        layers=layers,
+        architecture=architecture,
+        **kwargs,
+    )
     return model
 
 
-def iresnet18(pretrained=False, progress=True, **kwargs):
-    return _iresnet('iresnet18', IBasicBlock, [2, 2, 2, 2], pretrained,
-                    progress, **kwargs)
+def iresnet18(**kwargs):
+    """Constructs a ResNet-18 based model."""
+    return _iresnet(
+        block=IBasicBlock,
+        layers=[2, 2, 2, 2],
+        **kwargs,
+    )
 
 
-def iresnet34(pretrained=False, progress=True, **kwargs):
-    return _iresnet('iresnet34', IBasicBlock, [3, 4, 6, 3], pretrained,
-                    progress, **kwargs)
+def iresnet34(**kwargs):
+    """Constructs a ResNet-34 based model."""
+    return _iresnet(
+        block=IBasicBlock,
+        layers=[3, 4, 6, 3],
+        **kwargs,
+    )
 
 
-def iresnet50(pretrained=False, progress=True, **kwargs):
-    return _iresnet('iresnet50', IBasicBlock, [3, 4, 14, 3], pretrained,
-                    progress, **kwargs)
+def iresnet50(**kwargs):
+    """Constructs a ResNet-50 based model."""
+    return _iresnet(
+        block=IBasicBlock,
+        layers=[3, 4, 14, 3],
+        **kwargs,
+    )
 
 
-def iresnet100(pretrained=False, progress=True, **kwargs):
-    return _iresnet('iresnet100', IBasicBlock, [3, 13, 30, 3], pretrained,
-                    progress, **kwargs)
+def iresnet100(**kwargs):
+    """Constructs a ResNet-100 based model."""
+    return _iresnet(
+        block=IBasicBlock,
+        layers=[3, 13, 30, 3],
+        **kwargs,
+    )
 
 
-def iresnet200(pretrained=False, progress=True, **kwargs):
-    return _iresnet('iresnet200', IBasicBlock, [6, 26, 60, 6], pretrained,
-                    progress, **kwargs)
+def iresnet200(**kwargs):
+    """Constructs a ResNet-200 based model."""
+    return _iresnet(
+        block=IBasicBlock,
+        layers=[6, 26, 60, 6],
+        **kwargs,
+    )
--- recognition/arcface_torch/backbones/iresnet2060.py
+++ recognition/arcface_torch/backbones/iresnet2060.py
@@ -1,103 +1,151 @@
+"""ArcFace backbone with Resnet 2060."""
+from typing import Iterable, Callable
+
 import torch
 from torch import nn
 
-assert torch.__version__ >= "1.8.1"
+
+def check_torch_version(min_major: int, min_minor: int, min_patch: int):
+    """Make sure PyTorch version matches minimum, raises on failure."""
+    major, minor, patch = (int(x) for x in torch.__version__.split("."))
+    assert (
+        (major > min_major)
+        or (major == min_major and minor > min_minor)
+        or (major == min_major and minor == min_minor and patch >= min_patch)
+    ), "PyTorch version must be {}.{}.{} or higher".format(
+        min_major, min_minor, min_patch
+    )
+
+
+# Require PyTorch 1.8.1 or higher
+check_torch_version(1, 8, 1)
+
 from torch.utils.checkpoint import checkpoint_sequential
 
-__all__ = ['iresnet2060']
+__all__ = ["iresnet2060"]
 
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes,
-                     out_planes,
-                     kernel_size=3,
-                     stride=stride,
-                     padding=dilation,
-                     groups=groups,
-                     bias=False,
-                     dilation=dilation)
+    return nn.Conv2d(
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=dilation,
+        groups=groups,
+        bias=False,
+        dilation=dilation,
+    )
 
 
 def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
-    return nn.Conv2d(in_planes,
-                     out_planes,
-                     kernel_size=1,
-                     stride=stride,
-                     bias=False)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 
 class IBasicBlock(nn.Module):
+    """Basic Block for ResNet."""
+
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None,
-                 groups=1, base_width=64, dilation=1):
+    def __init__(
+        self,
+        in_planes,
+        planes,
+        stride=1,
+        downsample=None,
+        groups=1,
+        base_width=64,
+        dilation=1,
+    ):
         super(IBasicBlock, self).__init__()
         if groups != 1 or base_width != 64:
-            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+            raise ValueError("BasicBlock only supports groups=1 and base_width=64")
         if dilation > 1:
             raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
-        self.bn1 = nn.BatchNorm2d(inplanes, eps=1e-05, )
-        self.conv1 = conv3x3(inplanes, planes)
-        self.bn2 = nn.BatchNorm2d(planes, eps=1e-05, )
+        self.bn1 = nn.BatchNorm2d(
+            in_planes,
+            eps=1e-05,
+        )
+        self.conv1 = conv3x3(in_planes, planes)
+        self.bn2 = nn.BatchNorm2d(
+            planes,
+            eps=1e-05,
+        )
         self.prelu = nn.PReLU(planes)
         self.conv2 = conv3x3(planes, planes, stride)
-        self.bn3 = nn.BatchNorm2d(planes, eps=1e-05, )
+        self.bn3 = nn.BatchNorm2d(
+            planes,
+            eps=1e-05,
+        )
         self.downsample = downsample
         self.stride = stride
 
-    def forward(self, x):
-        identity = x
-        out = self.bn1(x)
+    def forward(self, input_tensor:torch.Tensor) -> torch.Tensor:
+        """Forward pass."""
+        identity = input_tensor
+        out = self.bn1(input_tensor)
         out = self.conv1(out)
         out = self.bn2(out)
         out = self.prelu(out)
         out = self.conv2(out)
         out = self.bn3(out)
         if self.downsample is not None:
-            identity = self.downsample(x)
+            identity = self.downsample(input_tensor)
         out += identity
         return out
 
 
 class IResNet(nn.Module):
+    """ResNet for ArcFace."""
+
     fc_scale = 7 * 7
 
-    def __init__(self,
-                 block, layers, dropout=0, num_features=512, zero_init_residual=False,
-                 groups=1, width_per_group=64, replace_stride_with_dilation=None, fp16=False):
+    def __init__(
+        self,
+        block,
+        layers,
+        dropout=0,
+        num_features=512,
+        zero_init_residual=False,
+        groups=1,
+        width_per_group=64,
+        replace_stride_with_dilation=None,
+        fp16=False,
+    ):
         super(IResNet, self).__init__()
         self.fp16 = fp16
-        self.inplanes = 64
+        self.in_planes = 64
         self.dilation = 1
         if replace_stride_with_dilation is None:
             replace_stride_with_dilation = [False, False, False]
         if len(replace_stride_with_dilation) != 3:
-            raise ValueError("replace_stride_with_dilation should be None "
-                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+            raise ValueError(
+                "replace_stride_with_dilation should be None "
+                "or a 3-element tuple, got {}".format(replace_stride_with_dilation)
+            )
         self.groups = groups
         self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(self.inplanes, eps=1e-05)
-        self.prelu = nn.PReLU(self.inplanes)
+        self.conv1 = nn.Conv2d(
+            3, self.in_planes, kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(self.in_planes, eps=1e-05)
+        self.prelu = nn.PReLU(self.in_planes)
         self.layer1 = self._make_layer(block, 64, layers[0], stride=2)
-        self.layer2 = self._make_layer(block,
-                                       128,
-                                       layers[1],
-                                       stride=2,
-                                       dilate=replace_stride_with_dilation[0])
-        self.layer3 = self._make_layer(block,
-                                       256,
-                                       layers[2],
-                                       stride=2,
-                                       dilate=replace_stride_with_dilation[1])
-        self.layer4 = self._make_layer(block,
-                                       512,
-                                       layers[3],
-                                       stride=2,
-                                       dilate=replace_stride_with_dilation[2])
-        self.bn2 = nn.BatchNorm2d(512 * block.expansion, eps=1e-05, )
+        self.layer2 = self._make_layer(
+            block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0]
+        )
+        self.layer3 = self._make_layer(
+            block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1]
+        )
+        self.layer4 = self._make_layer(
+            block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2]
+        )
+        self.bn2 = nn.BatchNorm2d(
+            512 * block.expansion,
+            eps=1e-05,
+        )
         self.dropout = nn.Dropout(p=dropout, inplace=True)
         self.fc = nn.Linear(512 * block.expansion * self.fc_scale, num_features)
         self.features = nn.BatchNorm1d(num_features, eps=1e-05)
@@ -116,61 +164,84 @@ class IResNet(nn.Module):
                 if isinstance(m, IBasicBlock):
                     nn.init.constant_(m.bn2.weight, 0)
 
-    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+    def _make_layer(
+        self,
+        block: nn.Module,
+        planes: Iterable,
+        blocks: int,
+        stride: int = 1,
+        dilate: bool = False,
+    ):
+        """Make a layer with a specific block"""
         downsample = None
         previous_dilation = self.dilation
         if dilate:
             self.dilation *= stride
             stride = 1
-        if stride != 1 or self.inplanes != planes * block.expansion:
+        if stride != 1 or self.in_planes != planes * block.expansion:
             downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                nn.BatchNorm2d(planes * block.expansion, eps=1e-05, ),
+                conv1x1(self.in_planes, planes * block.expansion, stride),
+                nn.BatchNorm2d(
+                    planes * block.expansion,
+                    eps=1e-05,
+                ),
             )
         layers = []
         layers.append(
-            block(self.inplanes, planes, stride, downsample, self.groups,
-                  self.base_width, previous_dilation))
-        self.inplanes = planes * block.expansion
+            block(
+                self.in_planes,
+                planes,
+                stride,
+                downsample,
+                self.groups,
+                self.base_width,
+                previous_dilation,
+            )
+        )
+        self.in_planes = planes * block.expansion
         for _ in range(1, blocks):
             layers.append(
-                block(self.inplanes,
-                      planes,
-                      groups=self.groups,
-                      base_width=self.base_width,
-                      dilation=self.dilation))
+                block(
+                    self.in_planes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                )
+            )
 
         return nn.Sequential(*layers)
 
-    def checkpoint(self, func, num_seg, x):
+    def checkpoint(self, func: Callable, n_segments: int, input_tensor: torch.Tensor):
+        """Checkpoints the output of a layer."""
         if self.training:
-            return checkpoint_sequential(func, num_seg, x)
+            return checkpoint_sequential(func, n_segments, input_tensor)
         else:
-            return func(x)
+            return func(input_tensor)
 
-    def forward(self, x):
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+        """Forward pass."""
         with torch.cuda.amp.autocast(self.fp16):
-            x = self.conv1(x)
-            x = self.bn1(x)
-            x = self.prelu(x)
-            x = self.layer1(x)
-            x = self.checkpoint(self.layer2, 20, x)
-            x = self.checkpoint(self.layer3, 100, x)
-            x = self.layer4(x)
-            x = self.bn2(x)
-            x = torch.flatten(x, 1)
-            x = self.dropout(x)
-        x = self.fc(x.float() if self.fp16 else x)
-        x = self.features(x)
-        return x
+            input_tensor = self.conv1(input_tensor)
+            input_tensor = self.bn1(input_tensor)
+            input_tensor = self.prelu(input_tensor)
+            input_tensor = self.layer1(input_tensor)
+            input_tensor = self.checkpoint(
+                func=self.layer2, n_segments=20, input_tensor=input_tensor
+            )
+            input_tensor = self.checkpoint(
+                func=self.layer3, n_segments=100, input_tensor=input_tensor
+            )
+            input_tensor = self.layer4(input_tensor)
+            input_tensor = self.bn2(input_tensor)
+            input_tensor = torch.flatten(input_tensor, 1)
+            input_tensor = self.dropout(input_tensor)
+        input_tensor = self.fc(input_tensor.float() if self.fp16 else input_tensor)
+        input_tensor = self.features(input_tensor)
+        return input_tensor
 
 
-def _iresnet(arch, block, layers, pretrained, progress, **kwargs):
-    model = IResNet(block, layers, **kwargs)
-    if pretrained:
-        raise ValueError()
+def iresnet2060(**kwargs):
+    """Creates a iResNet-2060 model."""
+    model = IResNet(block=IBasicBlock, layers=[3, 128, 1024 - 128, 3], **kwargs)
     return model
-
-
-def iresnet2060(pretrained=False, progress=True, **kwargs):
-    return _iresnet('iresnet2060', IBasicBlock, [3, 128, 1024 - 128, 3], pretrained, progress, **kwargs)
